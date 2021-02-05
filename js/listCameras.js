73d1ea026f8c25dd75ca4248917efa97fc90494f
Shigni <Louisservant86@gmail.com>
2021-02-05 16:04:59 +0100

    Cards + Footer

--- js/listCameras.js
+++ js/listCameras.js
@@ -9,11 +9,11 @@ async function main() {
         cameras.forEach(camera => {
             const $newTemplateCamera = $templateCamera.cloneNode(true);
             $newTemplateCamera.querySelector('.camera_name').innerText = camera.name
-            $newTemplateCamera.querySelector('.camera_lense').innerText = camera.lenses
+            $newTemplateCamera.querySelector('.camera_lense').innerText = 'Objectif: ' + camera.lenses[0]
             $newTemplateCamera.querySelector('.camera_image').src = camera.imageUrl
-            $newTemplateCamera.querySelector('.camera_id').innerText = camera.id
+            $newTemplateCamera.querySelector('.camera_id').innerText = 'Ref: ' + camera.id
             $newTemplateCamera.querySelector('.camera_description').innerText = camera.description
-            $newTemplateCamera.querySelector('.camera_price').innerText = camera.price
+            $newTemplateCamera.querySelector('.camera_price').innerText = camera.price + '€'
             
 
             $cameras.append($newTemplateCamera);
